8986b1f249681cc6c4d2d84f8ae34b9ee11fb77c
SnowO1127 <496800492@qq.com>
2016-03-28 17:05:07 +0800

    2016-03-28 17:04

---
+++ WMS/datasorce/sy_oganize.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="sy_oganize.ashx.cs" Class="WMS.datasorce.sy_oganize" %>
---
+++ WMS/datasorce/sy_oganize.ashx.cs
@@ -0,0 +1,125 @@
+﻿using BLL;
+using Common;
+using Model;
+using PageModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WMS.datasorce
+{
+    /// <summary>
+    /// sy_oganize 的摘要说明
+    /// </summary>
+    public class sy_oganize : IHttpHandler
+    {
+        private readonly SysOganizeBLL bll = new SysOganizeBLL();
+        JsonResult jr;
+        SysOganize so;
+        PageSysOganize pso;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            HttpRequest request = context.Request;
+
+            switch (request["action"])
+            {
+                case "getoganizetree":
+                    try
+                    {
+                        context.Response.Write(utils.SerializeObject(bll.GetOganizeTree()));
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                    break;
+                case "getoganize":
+                    try
+                    {
+                        context.Response.Write(utils.SerializeObject(bll.GetList()));
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+
+                    break;
+                case "getoganizebypage":
+                    try
+                    {
+                        pso = utils.AutoWiredClass<PageSysOganize>(request, pso = new PageSysOganize());
+                        context.Response.Write(utils.SerializeObject(bll.GetListByPage(pso)));
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+
+                    break;
+                case "addoganize":
+                    jr = new JsonResult();
+                    try
+                    {
+                        so = utils.AutoWiredClass<SysOganize>(request, so = new SysOganize());
+
+                        so.ID = Guid.NewGuid().ToString();
+                        so.CDate = DateTime.Now;
+
+                        bll.AddOganize(so);
+
+                        jr.Success = true;
+                        jr.Msg = "保存成功！";
+                    }
+                    catch (Exception ex)
+                    {
+                        jr.Msg = ex.ToString();
+                    }
+
+                    context.Response.Write(utils.SerializeObject(jr));
+                    break;
+                case "updateoganize":
+                    jr = new JsonResult();
+                    try
+                    {
+                        so = utils.AutoWiredClass<SysOganize>(request, so = new SysOganize());
+
+                        bll.UpdateOganize(so);
+                        jr.Success = true;
+                        jr.Msg = "保存成功！";
+                    }
+                    catch (Exception ex)
+                    {
+                        jr.Msg = ex.ToString();
+                    }
+
+                    context.Response.Write(utils.SerializeObject(jr));
+                    break;
+                case "getoneoganize":
+                    try
+                    {
+                        string id = request["id"];
+
+                        so = bll.GetOneOganize(id);
+
+                        context.Response.Write(utils.SerializeObject(so));
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                    break;
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
