273ae6242decc58ab447c7c6f4c6b2fa0f4ef862
SnowO1127 <496800492@qq.com>
2017-01-04 17:04:02 +0800

    添加IDAL

--- IDAL/Properties/AssemblyInfo.cs
+++ IDAL/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("4085204c-ec76-4f0a-ac0a-0935880f2c67")]
+[assembly: Guid("014d31bd-4b98-41fe-8249-363ede54cbbf")]
 
 // 程序集的版本信息由下面四个值组成: 
 //
